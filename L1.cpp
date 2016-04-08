c22a61c5cfe6c58d0907fb304bc65de5e8ba373d
vbas <vbas@cin.ufpe.br>
2016-04-08 02:47:18 -0300

    ia comecar mas deu mt sono

---
+++ L1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+
+// -------------------- interface .h -----------------------------------
+class galpao
+{
+	int total_boxes, total_weight_boxes;
+	vector<int> g;
+public:
+	galpao();
+	
+};
+
+// ------------------ implementacao .cpp --------------------------
