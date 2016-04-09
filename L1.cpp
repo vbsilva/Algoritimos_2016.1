674c672c45587118d0c98b1a3c7141eee70f6e52
vbas <vbas@cin.ufpe.br>
2016-04-09 03:25:22 -0300

    mais uma coisinha

--- L1.cpp
+++ L1.cpp
@@ -5,13 +5,73 @@ using namespace std;
 
 
 // -------------------- interface .h -----------------------------------
-class galpao
+//galpao
+class Galpao
 {
-	int total_boxes, total_weight_boxes;
-	vector<int> g;
+private:
+	int sum;
+	vector<int> vec;
+
 public:
-	galpao();
+	Galpao();
+	~Galpao();
+	int getNumItens();
+	void add(int w);
+
+};
+
+
+// caminhao
+class Caminhao
+{
+private:
+	int capacity, sum;
+	vector<int> vec;
+public:
+	Caminhao();
+	~Caminhao();
+	void loa(Galpao g);
 	
 };
 
 // ------------------ implementacao .cpp --------------------------
+//galpao
+Galpao::Galpao(){
+	sum = 0;
+}
+
+Galpao::~Galpao(){}
+
+int Galpao::getNumItens(){
+	return vec.size();
+}
+void Galpao::add(int w){
+	sum += w;
+	vec.push_back(w);
+	cout << vec.size() << " " << sum << endl;
+}
+
+//caminhao
+Caminhao::Caminhao(){
+	capacity = 0;
+	sum = 0;
+};
+Caminhao::~Caminhao(){};
+void Caminhao::loa(Galpao g){
+	cout << "ae" << endl;
+}
+// ------------------------ main .cpp ------------------------------
+
+int main(){
+
+	Galpao g;
+	g.add(10);
+	g.add(2);
+	int i;
+	cin >> i;
+	Caminhao *c = new Caminhao[i];
+	for(int j = 0; j < i; j++){
+		c[j].loa(g);
+	}
+	return 0;
+}
