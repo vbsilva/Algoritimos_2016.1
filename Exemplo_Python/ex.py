437cf8a3e0af96a06fe67a12b695d1e7385345df
vbas <vbas@cin.ufpe.br>
2016-04-18 02:52:04 -0300

    exemplo em python

---
+++ Exemplo_Python/ex.py
@@ -0,0 +1,16 @@
+class MyClass():
+	def __init__(self):
+		self._nome = ""
+	def setNome(self, nome):
+		self._nome = nome;
+	def getNome(self):
+		return self._nome
+
+
+def main():
+	x = MyClass()
+	x.setNome("olar mundo!")
+	print(x.getNome())
+
+if __name__ == "__main__":
+	main()
