2bb19fd7667b9a55b88d2b285288328e7cb995ad
vbsilva <vbas@cin.ufpe.br>
2016-04-26 01:44:45 -0300

    Update L3.cpp
    hashcode apenas

--- 2016.1/L3.cpp
+++ 2016.1/L3.cpp
@@ -1,16 +1,58 @@
 #include <bits/stdc++.h>
-//#include <boost/algorithm/string.hpp>
 using namespace std;
 
+class wordNode
+{
+private:
+	string word;
+	int hashcode;
+	vector<int> ocurrences;
+public:
+	wordNode(char *word);
+	~wordNode();
+	string getWord();
+	void setHashcode();
+	int getHashcode();
+
+	
+};
+string wordNode::getWord(){ return this->word; }
+
+void wordNode::setHashcode(){
+	int x = this->word.length();
+	for (int i = 1; i <= x; ++i){
+		this->hashcode += (this->word.at(i-1) - 'a')*(pow(26, x-i));
+	}
+	cout << this->hashcode << endl;
+}
+
+int wordNode::getHashcode(){ return this->hashcode; }
+
+wordNode::wordNode(char *word){
+	this->word = word;
+	this->setHashcode();
+	//this->hashcode = this->setHashcode()
+}
+
+
 int main(){
-	string str;
-	//vector<string> SplitVec;
-	getline(cin, str);
-	if(str == "$TEXTO"){
-		while(str != "$CONSULTAS"){
-			getline(cin, str);
-			//boost::split(SplitVec, str, boost::is_any_of(" "));
+	char line[100];
+	char *token;
+
+	scanf("%[^\n]", line);
+
+	if(!(strcmp(line, "$TEXTO"))){
+		while(true){
+			scanf(" %[^\n]", line);
+			token = strtok(line, " ");
+			while(token != NULL){
+				cout << token << endl;;
+				wordNode *w = new wordNode(token);
+				token = strtok(NULL, " ");
+			}
+			break;
+
 		}
 	}
-	return 0;
+return 0;
 }
